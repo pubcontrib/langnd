33f30db48c18cf387e5f0006cd7e84a0e466674c
Jason Kaiser <wareification@programmer.net>
2022-08-07 22:19:29 -0500

    release: set version to v0.1.0

--- src/utility.h
+++ src/utility.h
@@ -1,7 +1,7 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 
-#define PROGRAM_VERSION "v0.0.0"
+#define PROGRAM_VERSION "v0.1.0"
 #define PROGRAM_NAME "langnd"
 #define PROGRAM_SUCCESS 0
 #define PROGRAM_FAILURE 1
