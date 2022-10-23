9610945b6855c663a81be3484e0c6a905f9e73a5
Jason Kaiser <wareification@programmer.net>
2022-10-23 17:46:48 -0500

    test: add read stdin cases

--- test/suite/function/read.sh
+++ test/suite/function/read.sh
@@ -1,5 +1,29 @@
 suite 'function/read'
 
+verify '@read(0, null)' \
+    'with stdin' 'TEXT INPUT' \
+    'prints to stdout' ''
+verify '@write(@read(0, null), 1)' \
+    'with stdin' 'TEXT INPUT' \
+    'prints to stdout' 'TEXT INPUT'
+verify '@write(@read(0, null), 1)' \
+    'with stdin' 'LINE 1\nLINE 2\nLINE 3' \
+    'prints to stdout' 'LINE 1
+LINE 2
+LINE 3'
+verify '@read(0, "\n")' \
+    'with stdin' 'TEXT INPUT' \
+    'prints to stdout' ''
+verify '@write(@read(0, "\n"), 1)' \
+    'with stdin' 'TEXT INPUT' \
+    'prints to stdout' 'TEXT INPUT'
+verify '@write(@read(0, "\n"), 1)' \
+    'with stdin' 'LINE 1\nLINE 2\nLINE 3' \
+    'prints to stdout' 'LINE 1'
+verify '@write(@read(0, "\n"), 1) @write(@read(0, "\n"), 1) @write(@read(0, "\n"), 1) @write(@read(0, "\n"), 1)' \
+    'with stdin' 'LINE 1\nLINE 2\nLINE 3' \
+    'prints to stdout' 'LINE 1LINE 2LINE 3'
+
 if [ -z "$NO_PERSISTENCE" ]
 then
     file="${workspace}/read.txt"
@@ -28,8 +52,8 @@ LINE 3'
     verify '@write(@read("'"$file"'", "\n"), 1)' \
         'prints to stdout' 'LINE 1'
     printf 'LINE 1\nLINE 2\nLINE 3' > "$file"
-    verify '@write(@read("'"$file"'", "\n"), 1) @write(@read("'"$file"'", "\n"), 1)' \
-        'prints to stdout' 'LINE 1LINE 1'
+    verify '@write(@read("'"$file"'", "\n"), 1) @write(@read("'"$file"'", "\n"), 1) @write(@read("'"$file"'", "\n"), 1) @write(@read("'"$file"'", "\n"), 1)' \
+        'prints to stdout' 'LINE 1LINE 1LINE 1LINE 1'
 fi
 
 verify '@read(0, "")' \
