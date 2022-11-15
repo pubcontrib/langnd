12f813e232a1e61c057e7a79708277383749eb14
Jason Kaiser <wareification@programmer.net>
2022-11-14 22:09:14 -0600

    test: add cases for unknown token symbols

--- test/suite/token/unknown.sh
+++ test/suite/token/unknown.sh
@@ -4,3 +4,62 @@ verify 'unknown' \
     'errors with lex message' 'unknown'
 verify 'NULL' \
     'errors with lex message' 'NULL'
+
+verify "`printf '\001'`" \
+    'errors with lex message' "`printf '\001'`"
+verify "`printf '\002'`" \
+    'errors with lex message' "`printf '\002'`"
+verify "`printf '\003'`" \
+    'errors with lex message' "`printf '\003'`"
+verify "`printf '\004'`" \
+    'errors with lex message' "`printf '\004'`"
+verify "`printf '\005'`" \
+    'errors with lex message' "`printf '\005'`"
+verify "`printf '\006'`" \
+    'errors with lex message' "`printf '\006'`"
+verify "`printf '\007'`" \
+    'errors with lex message' "`printf '\007'`"
+verify "`printf '\010'`" \
+    'errors with lex message' "`printf '\010'`"
+verify "`printf '\013'`" \
+    'errors with lex message' "`printf '\013'`"
+verify "`printf '\014'`" \
+    'errors with lex message' "`printf '\014'`"
+verify "`printf '\016'`" \
+    'errors with lex message' "`printf '\016'`"
+verify "`printf '\017'`" \
+    'errors with lex message' "`printf '\017'`"
+verify "`printf '\020'`" \
+    'errors with lex message' "`printf '\020'`"
+verify "`printf '\021'`" \
+    'errors with lex message' "`printf '\021'`"
+verify "`printf '\022'`" \
+    'errors with lex message' "`printf '\022'`"
+verify "`printf '\023'`" \
+    'errors with lex message' "`printf '\023'`"
+verify "`printf '\024'`" \
+    'errors with lex message' "`printf '\024'`"
+verify "`printf '\025'`" \
+    'errors with lex message' "`printf '\025'`"
+verify "`printf '\026'`" \
+    'errors with lex message' "`printf '\026'`"
+verify "`printf '\027'`" \
+    'errors with lex message' "`printf '\027'`"
+verify "`printf '\030'`" \
+    'errors with lex message' "`printf '\030'`"
+verify "`printf '\031'`" \
+    'errors with lex message' "`printf '\031'`"
+verify "`printf '\032'`" \
+    'errors with lex message' "`printf '\032'`"
+verify "`printf '\033'`" \
+    'errors with lex message' "`printf '\033'`"
+verify "`printf '\034'`" \
+    'errors with lex message' "`printf '\034'`"
+verify "`printf '\035'`" \
+    'errors with lex message' "`printf '\035'`"
+verify "`printf '\036'`" \
+    'errors with lex message' "`printf '\036'`"
+verify "`printf '\037'`" \
+    'errors with lex message' "`printf '\037'`"
+verify "`printf '\177'`" \
+    'errors with lex message' "`printf '\177'`"
