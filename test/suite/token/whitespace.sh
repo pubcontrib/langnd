f1c1a2f5e9df8672fa08d7ca8c2cb74b5f0b8ca1
Jason Kaiser <wareification@programmer.net>
2022-11-14 21:50:28 -0600

    test: add cases for supported whitespace tokens

--- test/suite/token/whitespace.sh
+++ test/suite/token/whitespace.sh
@@ -2,3 +2,11 @@ suite 'token/whitespace'
 
 verify '' \
     'prints to stdout' ''
+verify ' ' \
+    'prints to stdout' ''
+verify "`printf '\011'`" \
+    'prints to stdout' ''
+verify "`printf '\012'`" \
+    'prints to stdout' ''
+verify "`printf '\015'`" \
+    'prints to stdout' ''
